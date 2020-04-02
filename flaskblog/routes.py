23ada9c300bcf1428b5b286dc2039f4d0ff9a6b0
yoondor <yoonjakaotic@gmail.com>
2020-04-02 15:49:22 +0900

    update, delete posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,28 +1,19 @@
-from flaskblog.models import User, Post
-from flask import render_template, url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
-from flaskblog import app, db, bcrypt
-from flask_login import login_user, current_user, logout_user, login_required
-from PIL import Image
-import secrets, os
+import os
+import secrets
 
-posts = [
-    {'author': 'Yoondor',
-     'title': 'what should I eat for dinner',
-     'content': 'no more pasta!',
-     'date_posted': 'March 12, 2020'
-     },
-    {'author': 'dummy',
-     'title': 'post2 title',
-     'content': 'Second post content',
-     'date_posted':'March 13, 2020'
-     }
-]
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request, abort
+from flask_login import login_user, current_user, logout_user, login_required
+
+from flaskblog import app, db, bcrypt
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flaskblog.models import User, Post
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -68,15 +59,16 @@ def logout():
     logout_user()
     return redirect(url_for('hello'))
 
-#saving the file itself, returns the file name
+
+# saving the file itself, returns the file name
 def save_pic(form_picture):
-    #create random hex
+    # create random hex
     ran_hex = secrets.token_hex(8)
     # if you're not using the variable, just use underscore
     _, f_ext = os.path.splitext(form_picture.filename)
     pic_fn = ran_hex + f_ext
     pic_path = os.path.join(app.root_path, 'static/profile_pics', pic_fn)
-    #resizing before saving
+    # resizing before saving
     output_size = (250, 250)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
@@ -106,3 +98,54 @@ def account():
     image_file = url_for('static', filename='profile_pics/'+current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
 
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created :3', 'success')
+        return redirect(url_for('hello'))
+    return render_template('post_create.html', title='글쓰기', legend = '글쓰기', form=form)
+
+
+@app.route("/post/int:<post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/int:<post_id>/update", methods=['GET', 'POST'])
+@login_required
+def post_update(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Posts been updated :D', 'success')
+        return redirect(url_for('post', post_id=post_id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
+    # note because I keep forgetting: title is what appears on the tab
+    return render_template('post_create.html', title='글 고치기', legend='글 고치기', form=form)
+
+
+@app.route("/post/int:<post_id>/delete", methods=['POST'])
+@login_required
+def post_delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your poor post has been murdered!', 'success')
+    return redirect(url_for('hello'))
